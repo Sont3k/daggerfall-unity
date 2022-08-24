575b465eeeeac43fde0f1f4b41154de5eb424f03
Kirill Vovkotrub <markus.residence@gmail.com>
2022-08-24 18:25:51 +0300

    feat: implement health slider and health script for pet

---
+++ Assets/Scripts/Game/Player/Pet/PetHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using DaggerfallWorkshop.Game.Entity;
+using UnityEngine.UI;
+
+namespace DaggerfallWorkshop.Game
+{
+    public class PetHealth : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private DaggerfallEntityBehaviour _daggerfallEntityBehaviour;
+        [SerializeField] private Slider _healthSlider;
+
+        private PlayerPetEntity _petEntity;
+        private readonly float _minHealth = 0f;
+
+        private void Start()
+        {
+            _petEntity = _daggerfallEntityBehaviour.Entity as PlayerPetEntity;
+
+            IncreaseHealth(50); // for testing purposes
+            DecreaseHealth(10);
+        }
+
+        private void UpdateHealthBar()
+        {
+            _healthSlider.minValue = _minHealth;
+            _healthSlider.value = _petEntity.CurrentHealth;
+            _healthSlider.maxValue = _petEntity.MaxHealth;
+        }
+
+        // Not used for now but can be used in future to increase health with UI update
+        public void IncreaseHealth(int amount)
+        {
+            _petEntity.IncreaseHealth(amount);
+            UpdateHealthBar();
+        }
+
+        // Not used for now but can be used in future to decrease health with UI update
+        public void DecreaseHealth(int amount)
+        {
+            _petEntity.DecreaseHealth(amount);
+            UpdateHealthBar();
+        }
+    }
+}
